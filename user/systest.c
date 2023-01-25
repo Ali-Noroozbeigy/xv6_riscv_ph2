f74bf44ab8a1374b618eb971fccdef63c09297e3
Ali-Noroozbeigy <ali.noroozbeigy@gmail.com>
2023-01-25 22:45:10 +0330

    add forktest

--- user/systest.c
+++ user/systest.c
@@ -1,11 +1,49 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
-#include "kernel/proc.h"
-#include "kernel/param.h"
-//#include "sys/types.h"
-//#include "unistd.h"
-//#include "time.h"
+
+#define N  1000
+
+void
+print(const char *s)
+{
+    write(1, s, strlen(s));
+}
+
+void
+forktest(void)
+{
+    int n, pid;
+
+    print("fork test\n");
+
+    for(n=0; n<N; n++){
+        pid = fork();
+        if(pid < 0)
+            break;
+        if(pid == 0)
+            exit(0);
+    }
+
+    if(n == N){
+        print("fork claimed to work N times!\n");
+        exit(1);
+    }
+
+    for(; n > 0; n--){
+        if(wait(0) < 0){
+            print("wait stopped early\n");
+            exit(1);
+        }
+    }
+
+    if(wait(0) != -1){
+        print("wait got too many\n");
+        exit(1);
+    }
+
+    print("fork test OK\n");
+}
 
 int main(){
     int ticket_for_test = 50;
@@ -14,11 +52,12 @@ int main(){
         exit(-1);
     }
 
-    struct processes_info *p;
-
-    getprocessesinfo(p);
-
-    printf("number of process: %d\n", p->num_processes);
+    forktest();
+//    struct processes_info p;
+//
+//    getprocessesinfo(&p);
+//
+//    printf("number of process: %d\n", p.num_processes);
 
 
 
