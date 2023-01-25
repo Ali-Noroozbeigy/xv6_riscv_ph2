a9042d34d504a99c0daf717081bb52c0d582d618
Ali-Noroozbeigy <ali.noroozbeigy@gmail.com>
2023-01-25 10:00:07 +0330

    add prototype getprocessesinfo edited defs.h

--- kernel/defs.h
+++ kernel/defs.h
@@ -8,6 +8,7 @@ struct spinlock;
 struct sleeplock;
 struct stat;
 struct superblock;
+struct processes_info;
 
 // bio.c
 void            binit(void);
@@ -107,6 +108,7 @@ int             either_copyout(int user_dst, uint64 dst, void *src, uint64 len);
 int             either_copyin(void *dst, int user_src, uint64 src, uint64 len);
 void            procdump(void);
 int             settickets(int);
+int             getprocessesinfo(struct processes_info* p);
 
 // swtch.S
 void            swtch(struct context*, struct context*);
