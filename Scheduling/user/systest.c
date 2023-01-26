cb8e74bd3b5202ae3d1db55421478465e93a6848
Ali-Noroozbeigy <ali.noroozbeigy@gmail.com>
2023-01-26 23:03:33 +0330

    edit systest

--- Scheduling/user/systest.c
+++ Scheduling/user/systest.c
@@ -4,7 +4,7 @@
 #include "kernel/info.h"
 #include "kernel/param.h"
 
-#define N  1000
+#define N  10
 
 void
 print(const char *s)
@@ -31,14 +31,10 @@ forktest(void)
         }
     }
 
-    struct processes_info p;
-    getprocessesinfo(&p);
-    printf("***************************\n");
-
-    if(n == N){
-        print("fork claimed to work N times!\n");
-        exit(1);
-    }
+//    if(n == N){
+//        print("fork claimed to work N times!\n");
+//        exit(1);
+//    }
 
     for(; n > 0; n--){
         if(wait(0) < 0){
@@ -62,26 +58,33 @@ int main(){
         exit(-1);
     }
 
-//    forktest();
 
-//    struct processes_info p;
-//    printf("%p", &p);
-//    getprocessesinfo(&p);
-
-//    printf("number of process: %d\n", p.num_processes);
-//    for (int i=0; i<NPROC; i++){
-//        printf("pid[%d]: %d\n", i, p.pids[i]);
-//        printf("tickets[%d]: %d\n", i, p.tickets[i]);
-//        printf("ticks[%d]: %d\n", i, p.ticks[i]);
-//    }
-
-/* part without bug */
     struct processes_info p;
     getprocessesinfo(&p);
-    printf("***************************\n");
-    forktest();
-    printf("***************************\n");
-    //getprocessesinfo(&p);
+
+    printf("number of process: %d\n", p.num_processes);
+    for (int i=0; i<p.num_processes; i++){
+        printf("pid[%d]: %d\n", i, p.pids[i]);
+        printf("tickets[%d]: %d\n", i, p.tickets[i]);
+        printf("ticks[%d]: %d\n", i, p.ticks[i]);
+    }
+
+    if(fork()==0){
+        int x = 0;
+        for(int i=0; i<10000000 ; i++)
+            x+=1;
+    }
+
+    else{
+        getprocessesinfo(&p);
+
+        printf("number of process: %d\n", p.num_processes);
+        for (int i = 0; i < p.num_processes; i++) {
+            printf("pid[%d]: %d\n", i, p.pids[i]);
+            printf("tickets[%d]: %d\n", i, p.tickets[i]);
+            printf("ticks[%d]: %d\n", i, p.ticks[i]);
+        }
+    }
 
     exit(0);
 }
