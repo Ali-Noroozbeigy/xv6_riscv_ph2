51f4b83cbd91f4e5d12b472014a0f8aa39e9e9d5
mahdi teimouri <74227366+mhditeimouri@users.noreply.github.com>
2023-01-26 19:20:16 +0330

    debug uthread
    thread switch arguments change

--- Thread/user/uthread.c
+++ Thread/user/uthread.c
@@ -81,7 +81,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
-    thread_switch(t->context,current_thread->context);
+    thread_switch(&t->context,&current_thread->context);
     t->state = RUNNABLE;
   } else
     next_thread = 0;
