5388e0cfcbaa6e8ddf9ca0a55dc4d6be798ca708
Ali-Noroozbeigy <ali.noroozbeigy@gmail.com>
2023-01-26 23:08:53 +0330

    edit testford

--- Scheduling/user/systest.c
+++ Scheduling/user/systest.c
@@ -4,53 +4,6 @@
 #include "kernel/info.h"
 #include "kernel/param.h"
 
-#define N  10
-
-void
-print(const char *s)
-{
-    write(1, s, strlen(s));
-}
-
-void
-forktest(void)
-{
-    int n, pid;
-
-    print("fork test\n");
-
-    for(n=0; n<N; n++){
-        pid = fork();
-        int x = 0;
-        if(pid < 0)
-            break;
-        if(pid == 0){
-            for(int f=0; f<10; f++)
-                x+=1;
-            exit(0);
-        }
-    }
-
-//    if(n == N){
-//        print("fork claimed to work N times!\n");
-//        exit(1);
-//    }
-
-    for(; n > 0; n--){
-        if(wait(0) < 0){
-            print("wait stopped early\n");
-            exit(1);
-        }
-    }
-
-    if(wait(0) != -1){
-        print("wait got too many\n");
-        exit(1);
-    }
-
-    print("fork test OK\n");
-}
-
 int main(){
     int ticket_for_test = 50;
 
